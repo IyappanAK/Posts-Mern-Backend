4e69da3b2d1ee1ffdd6f62d3a2684c9a7aac8d5b
Iyappan <117816671+IyappanAK@users.noreply.github.com>
2023-03-08 16:48:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://iyappan:aki861059@cluster0.y1y0ho9.mongodb.net/test";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+  
